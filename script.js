0441c2d7ea23426fbf2ace5964741affd3a6668a
hoangalex9603 <163833995+hoangalex9603@users.noreply.github.com>
2024-11-02 03:35:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const message = document.getElementById("message");
+    message.textContent = "Học tập không phải là để hoàn hảo, mà là để tốt hơn mỗi ngày.";
+    message.classList.remove("hidden");
+}
